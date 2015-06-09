328700b081fe6ae58fac8d69bd55f542c323d75b
Bruno Azenha Goncalves <azenha.bruno@gmail.com>
2015-06-08 22:32:22 -0300

    Adiciona SignUp servlet.
    * Cria o SignUpController.java
    * Redireciona o form de signup do signup.html para o servlet
    * Adiciona mapeamento do servlet no web.xml

---
+++ WEB-INF/classes/Bridgeport/SignUpController.java
@@ -0,0 +1,34 @@
+package Bridgeport;
+
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*; 
+import java.util.*; 
+import java.lang.Integer;
+
+
+public class SignUpController extends HttpServlet {
+
+	public void doGet (HttpServletRequest request, HttpServletResponse response){
+		execute(request, response);
+	}
+
+	public void doPost (HttpServletRequest request, HttpServletResponse response){
+		execute(request, response);
+	}
+    
+    public void execute(HttpServletRequest request, HttpServletResponse response){
+     
+        try{
+						
+		HttpSession session = request.getSession();
+        String url="error.jsp";
+            
+        RequestDispatcher dispatcher = request.getRequestDispatcher("../"+url);
+		dispatcher.forward(request, response);
+        
+        }catch(Exception e){
+			e.printStackTrace();
+		}
+    }
+}
